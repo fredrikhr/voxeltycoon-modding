cc468cce2ecc74cc77e2de16b028f2d0fa63fe0d
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2024-06-03 09:30:36 +0200

    Initialize repository for VoxelTycoon mod development

---
+++ mods/FredrikHr.VoxelTycoonModding.HelloVoxelWorld/HelloVoxelWorldMod.cs
@@ -0,0 +1,11 @@
+using VoxelTycoon.Modding;
+
+namespace FredrikHr.VoxelTycoonModding.HelloVoxelWorld;
+
+public class HelloVoxelWorldMod : Mod
+{
+    protected override void OnGameStarted()
+    {
+        base.OnGameStarted();
+    }
+}
